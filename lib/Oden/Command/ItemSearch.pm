f7d3fae8f79a53eefea0fa4e774826520ffead72
likkradyus <git@li.que.jp>
2022-06-01 20:56:02 +0900

    fixed typo.

--- lib/Oden/Command/ItemSearch.pm
+++ lib/Oden/Command/ItemSearch.pm
@@ -8,11 +8,11 @@ use Oden::Model::Item;
 
 =head1 NAME
 
-  Oden::Command::ItemSearch - item search from loadstone.
+  Oden::Command::ItemSearch - item search from lodestone.
 
 =head1 DESCRIPTION
 
-  Oden::Command::ItemSearch is provide URL of loadston from item name.
+  Oden::Command::ItemSearch is provide URL of lodeston from item name.
 
 =cut
 
@@ -33,7 +33,7 @@ sub run {
     if(my $item = Oden::Model::Item->lookup_item_by_name_ja($hear)){
         my $lodestone_url   = $item->lodestone_url;
         my $miraprisnap_url = $item->miraprisnap_url;
-        $talk .= sprintf("loadstone: %s\n",   $lodestone_url)   if $lodestone_url;
+        $talk .= sprintf("lodestone: %s\n",   $lodestone_url)   if $lodestone_url;
         $talk .= sprintf("miraprisnap: %s\n", $miraprisnap_url) if $miraprisnap_url;
     }
     return $talk;
--- lib/Oden/Model/Item.pm
+++ lib/Oden/Model/Item.pm
@@ -8,42 +8,59 @@ use URI::Escape;
 
 =head1 NAME
 
-  Oden::Model::Item - lookup item loadstone by name_ja.
+  Oden::Model::Item - lookup item lodestone by name_ja.
 
 =head1 DESCRIPTION
 
-  Oden::Model::Item is provide URL of loadston from item name.
+  Oden::Model::Item is provide URL of lodeston from item name.
 
 =cut
 
 =head1 CONSTRUCTOR AND STARTUP
 
+=head1 new
+
+  Creates and returns a Item Object
+
+=cut
+
+sub new {
+    my ($class) = @_;
+
+    my $self = bless {}, $class;
+    $self->build;
+    return $self;
+}
+
+sub build {
+    my $self = shift;
+    $self->{data_dir}           = "./data";
+    $self->{name_ja_to_item_id} = do sprintf("%s/name_ja_to_item_id.pl", $self->{data_dir})
+      or die 'cant read name_ja_to_item_id';
+}
+
 =head2 lookup_item_id_by_name_ja
 
-  Creates and returns a Item object.
+  Creates and returns a Item object from name_ja
 
 =cut
 
 # TODO: name_en, de, and fr.
+#
 sub lookup_item_by_name_ja {
-    my ($class, $name_ja) = @_;
-
+    my ($invocant, $name_ja) = @_;
     return unless $name_ja;
-    my $self = bless {}, $class;
-    $self->build;
+
+    my $self = ref $invocant eq 'Oden::Model::Item'
+      ? $invocant
+      : $invocant->new;
+    ;
 
     # tentative ( encoded utf8 or flagged utf8, that is the question)
     $self->{name_ja} = $name_ja;
     $self->{id}      = $self->{name_ja_to_item_id}->{Encode::encode_utf8($name_ja)}
       or return;
     return $self;
-}
-
-sub build {
-    my $self = shift;
-    $self->{base_dir}           = "$FindBin::Bin/";
-    $self->{data_dir}           = sprintf("%s/data", $self->{base_dir} );
-    $self->{name_ja_to_item_id} = do sprintf("%s/name_ja_to_item_id.pl", $self->{data_dir});
 }
 
 =head1 METHDOS
@@ -55,11 +72,12 @@ sub build {
 sub lodestone_url {
     my $self = shift;
     my $data_dir = $self->{data_dir};
-    # TODO: should not use awk.
 
-    my $find_loadstone_item_id_command = sprintf("awk 'NR==%s' %s/%s", $self->{id}, $self->{data_dir}, 'lodestone-item-id.txt');
-    my $lodestone_id = `$find_loadstone_item_id_command`;
+    # TODO: should not use awk.
+    my $find_lodestone_item_id_command = sprintf("awk 'NR==%s' %s/%s", $self->{id}, $self->{data_dir}, 'lodestone-item-id.txt');
+    my $lodestone_id = `$find_lodestone_item_id_command`;
     chomp $lodestone_id;
+
     return unless $lodestone_id;
     return sprintf("https://jp.finalfantasyxiv.com/lodestone/playguide/db/item/%s/", $lodestone_id);
 }
