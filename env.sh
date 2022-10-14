4a4854be6c59a2a0f6c50c60d4d43db068654f4b
likkradyus <git@li.que.jp>
2022-10-14 18:08:10 +0900

    別パスからの実行からに考慮する

--- env.sh
+++ env.sh
@@ -3,10 +3,11 @@ set -e
 
 export EDITOR=vim;
 export PATH="local/bin:$PATH"
-export PERL5LIB="local/lib/perl5:./lib"
+export BASE_DIR=$(cd $(dirname $0); pwd)
+
+export PERL5LIB="$BASE_DIR/local/lib/perl5:$BASE_DIR/lib"
 export LANG="C";
 
-export BASE_DIR=$(cd $(dirname $0); pwd)
 export DATA_DIR=${BASE_DIR}/data
 
 exec "$@"
